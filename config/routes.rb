d219a608dc18c1d211d4df64a48382351b6448d3
kanchimatsumoto <kanchi0707@outlook.jp>
2018-12-13 19:51:00 +0900

    edit_for_conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
-  get 'mypage' => 'users#index'
+  get 'mypage' => 'users#mypage'
   get 'mypage/card' => 'users#card'
   get 'mypage/card/add' => 'users#add'
 end
