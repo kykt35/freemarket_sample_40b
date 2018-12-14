4a855116cc822df7cdf9c16b4eaee4e0ce81396a
Ttoshinari <freeworker1105@gmail.com>
2018-12-14 16:12:36 +0900

    ログアウト画面実装２

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
   resources :items, only: [:new]
-  get 'users', to: 'users#logout'
+  get 'users/logout', to: 'users#logout'
 end
