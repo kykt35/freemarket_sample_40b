189ea5f7a0a834467f2dc4b0bdd6b36073dafc24
kykt35 <kykt35@gmail.com>
2018-12-15 14:48:11 +0900

    Create items.js to add item image slider
    商品詳細ページの複数画像のスライド表示機能を作成

---
+++ app/assets/javascripts/items.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $('.item-photo').slick({
+      dots: true,
+      infinite: false,
+      arrows: false,
+      slidesToShow: 1,
+      pauseOnDotsHover: true,
+      customPaging: function(slider, i) {
+        var thumbSrc = $(slider.$slides[i]).attr('src');
+        return '<img src="' + thumbSrc + '">';
+      }
+  });
+
+  $('.slick-dots li').on({'mouseenter' : function(){
+      var i = $(this).index();
+      $('.item-photo').slick('slickGoTo', i, false);}
+    });
+});
+
