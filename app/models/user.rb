8a9a3cd8bf3b585aa8d5ccc1e674666f680f0934
kukbkub <kukbkub.create.program@gmail.com>
2018-12-17 19:45:19 +0900

    Create validates  for nickname ,and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :nickname, presence: true
+  validates_format_of :password, :with => /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/, :message => "は6文字以上の英数混在で入力してください。"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
