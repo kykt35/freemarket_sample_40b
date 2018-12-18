89511344fd66e7b12da859639f6dba6d0946ce9e
kykt35 <kykt35@gmail.com>
2018-12-18 10:33:39 +0900

    Add Prefecture and Leadtime seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,12 +53,25 @@ shippings.each do |ship|
 end
 
 #postage_selests table
-postage_selests table = %w(着払い(購入者負担) 送料込み(出品者負担))
+postage_selests = ["着払い(購入者負担)", "送料込み(出品者負担)"]
 postage_selests.each do |post|
-  postage = Postage_selest.where(name: post).first_or_initialize(name: post)
+  postage = PostageSelect.where(name: post).first_or_initialize(name: post)
   postage.save
 end
 
+#Prefecture table
+prefectures = %w(北海道 青森県 岩手県 宮城県 秋田県 山形県 福島県 茨城県
+栃木県 群馬県 埼玉県 千葉県 東京都 神奈川県 新潟県 富山県 石川県 福井県
+山梨県 長野県 岐阜県 静岡県 愛知県 三重県 滋賀県 京都府 大阪府 兵庫県 奈良県
+和歌山県 鳥取県 島根県 岡山県 広島県 山口県 徳島県 香川県 愛媛県 高知県
+福岡県 佐賀県 長崎県 熊本県 大分県 宮崎県 鹿児島県 沖縄県)
+prefectures.each do |pref|
+  prefecture = Prefecture.where(name: pref).first_or_initialize(name: pref)
+  prefecture.save
+end
 
-
-
+#Leadtime table
+["1~2日で発送", "2~3日で発送", "4~7日で発送"].each do |ltime|
+  leadtime = Leadtime.where(leadtime: ltime).first_or_initialize(leadtime: ltime)
+  leadtime.save
+end
