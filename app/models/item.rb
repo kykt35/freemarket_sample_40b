80cd055855df400c33085ff2020128142f24e179
kykt35 <kykt35@gmail.com>
2018-12-19 00:20:35 +0900

    Edit item associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ApplicationRecord
   has_many_attached :images
-  belongs_to :user
-  belongs_to :brand
+  belongs_to :seller, class_name: "User"
+  belongs_to :brand, optional: true
   belongs_to :category
-  belongs_to :size
+  belongs_to :size, optional: true
   belongs_to :item_condition
   belongs_to :shipping
   belongs_to :postage_select
