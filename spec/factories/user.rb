6f0f835d9c3109fcfa98a3151cb84ffb2e965521
kukbkub <kukbkub.create.program@gmail.com>
2018-12-18 16:35:54 +0900

    Update user.rb

---
+++ spec/factories/user.rb
@@ -0,0 +1,9 @@
+Factory.define do
+
+  factory :user do
+    password = "0a0a0a"
+    nickname Faker::Name.last_name
+    email    Faker::Internet.free_email
+    password password
+    password_confimation password
+end
