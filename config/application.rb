18202a77689ac6a4c4a6fadcbf5ba82e76cc0029
kukbkub <kukbkub.create.program@gmail.com>
2018-12-18 18:26:54 +0900

    Correct config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,9 @@ module FreemarketSample40b
       g.javascripts false
       g.helper false
       g.test_framework false
-      config.i18n.default_locale = :ja
+      if Rails.env != "test"
+        config.i18n.default_locale = :ja
+      end
     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
