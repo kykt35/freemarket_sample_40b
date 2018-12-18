898875a5d78c82d26a57da73b5b4516b25f87461
kykt35 <kykt35@gmail.com>
2018-12-18 14:08:54 +0900

    Add associations to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
   has_many_attached :images
+  belongs_to :user
+  belongs_to :brand
+  belongs_to :category
+  belongs_to :size
+  belongs_to :item_condition
+  belongs_to :shipping
+  belongs_to :postage_select
+  belongs_to :leadtime
+  belongs_to :prefecture
 end
