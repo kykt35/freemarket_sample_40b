0a33c195ff129b8243efba92e68e7bd7c4d0bc6f
kukbkub <kukbkub.create.program@gmail.com>
2018-12-18 19:33:15 +0900

    Delete conflict

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,7 +31,6 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -60,8 +59,6 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
-<<<<<<< HEAD
-=======
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
