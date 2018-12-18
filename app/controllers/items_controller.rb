12a626515a73a0b43f80e0d278452be7f353c86d
kykt35 <kykt35@gmail.com>
2018-12-18 14:21:40 +0900

    Add create action to  items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,12 @@ class ItemsController < ApplicationController
   def new
     #商品登録
   end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      respond_to do |format|
+        format.html { render :new}
+        format.json
+      end
+  end
 end
