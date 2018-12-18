17fdd0663b49f297a6d0866b0816803a36c4064b
kykt35 <kykt35@gmail.com>
2018-12-18 20:26:37 +0900

    Edit item_controller to add strongparameter

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,24 @@
 class ItemsController < ApplicationController
   def new
+    @item = Item.new()
     #商品登録
   end
   def create
     @item = Item.new(item_params)
     if @item.save
       respond_to do |format|
-        format.html { render :new}
+        format.html
         format.json
       end
+    else
+      render :new
+    end
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :brand_id, :size_id, :item_condition_id, :postage_select_id, :shipping_id, :prefecture_id,:leadtime_id, :price, :images).merge(seller_id: current_user.id)
+  end
+
 end
