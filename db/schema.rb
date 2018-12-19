e73a3ba48310d08b1bf491cc2fef9c0fd1aed8eb
kukbkub <kukbkub.create.program@gmail.com>
2018-12-19 16:12:04 +0900

    Remove columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_12_18_065539) do
+ActiveRecord::Schema.define(version: 2018_12_19_062634) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
