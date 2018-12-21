074a83a5fc1cf92ec426f10e0ddaf521ab169810
kykt35 <kykt35@gmail.com>
2018-12-21 17:44:03 +0900

    Fix association dependent:

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,5 +1,5 @@
 class Shipping < ApplicationRecord
   has_many :items
-  has_many :postage_selects_shippings, dependent :destroy
+  has_many :postage_selects_shippings, dependent: :destroy
   has_many :postage_select, through: :postage_selects_shippings
 end
