4e0448775bf3d71d3b6667b5a5ab04a7b60b9274
kykt35 <kykt35@gmail.com>
2018-12-22 00:13:09 +0900

    edit deploy setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set :application, 'freemarket_sample_40b'
 set :repo_url,  'git@github.com:kykt35/freemarket_sample_40b.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, %w{config/master.key}
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
@@ -15,7 +16,6 @@ set :ssh_options, auth_methods: ['publickey'],
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
-set :linked_files, %w{config/master.key}
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
