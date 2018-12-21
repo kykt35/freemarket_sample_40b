ea92f501b40177922b67782dbfa31f5dd3f0e24a
kykt35 <kykt35@gmail.com>
2018-12-21 17:48:52 +0900

    Add postage_select select action
    配送料の負担を選択すると配送方法が表示されるjsを追加

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -108,4 +108,62 @@ $(document).on('turbolinks:load', function() {
       console.log("fail");
     })
   });
+
+
+
+    // 配送料の負担選択で配送方法
+  $(document).on("change", "#item_postage_select_id", function (e) {
+    e.preventDefault();
+    var ps_id = $('#item_postage_select_id option:selected').val()
+    if (ps_id == ""){
+      $('#item_shipping_id').closest('.form-group').remove();
+      return;
+    }
+    $.ajax({
+      type: "GET",
+      url: '/postage_selects',
+      data: {id: ps_id},
+      dataType: 'json',
+      processData: true,
+      contentType: false,
+      timeout: 60000
+    })
+    .done(function(datas){
+      if ( $('#item_shipping_id').length == 0){
+        var formgroup =
+          `<div class="form-group">
+            <label for="item_shipping_id">配送方法
+              <span class="form-require">必須</span>
+            </label>
+            <div class="select-wrap">
+              <select class="select-default" name="item[shipping_id]" id="item_shipping_id">
+                <option value="">---</option>
+              </select>
+              <i class="fa fa-angle-down"></i>
+            </div>
+            <ul class="has-error-text">
+            </ul>
+          </div>`
+        options = datas.map(function(data){
+          option = $('<option>',{value: data.id, text: data.text});
+          return option;
+        });
+        $('#item_postage_select_id').parents('.form-group').after(formgroup
+        );
+        $('#item_shipping_id').append(options);
+
+      }else{
+        select = $('#item_shipping_id');
+        select.children().remove()
+        options = datas.map(function(data){
+          option = $('<option>',{value: data.id, text: data.text});
+          return option;
+        });
+        $('#item_shipping_id').append(options);
+      }
+    })
+    .fail(function(){
+      console.log("fail");
+    })
+  });
 });
