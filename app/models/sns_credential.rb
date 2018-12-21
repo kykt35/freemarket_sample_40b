acb4939f06fe1cd5788ad0f83b0856f05295c34a
kukbkub <kukbkub.create.program@gmail.com>
2018-12-21 20:24:36 +0900

    Derete unnecessary method of sns_credential.rb

--- app/models/sns_credential.rb
+++ app/models/sns_credential.rb
@@ -2,12 +2,4 @@ class SnsCredential < ApplicationRecord
   belongs_to :user
   validates_presence_of :user_id, :uid, :provider
   validates_uniqueness_of :uid, uniqueness: {:scope => :provider}
-  def self.find_with_omniauth(auth)
-    find_by(uid: auth['uid'], provider: auth['provider'])
-  end
-
-  def self.create_with_omniauth(auth)
-    user ||= User.create_from_auth!(auth)
-    self.create!(:user => user, :uid => auth['uid'], :provider => auth['provider'])
-  end
 end
