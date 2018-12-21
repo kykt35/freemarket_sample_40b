16d208da840889f75e460818926ccd5ad67beedf
kykt35 <kykt35@gmail.com>
2018-12-21 20:49:35 +0900

    Add size brand display and price calc
    サイズとブランドを表示する機能追加
    手数料、利益の表示を追加

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -109,7 +109,68 @@ $(document).on('turbolinks:load', function() {
     })
   });
 
+    // カテゴリ選択で必要な時にサイズとブランドを表示
+  $(document).on("change", "#item_category_id", function (e) {
+    e.preventDefault();
+    var category_id = $('#item_category_id option:selected').val()
+    if (category_id == ""){
+      $('#item_size_id').parent().parent().remove();
+      $('#item_brand_id').parent().parent().remove();
+      return;
+    }
+    $.ajax({
+      type: "GET",
+      url: '/categories/size_brand',
+      data: {id: category_id},
+      dataType: 'json',
+      processData: true,
+      contentType: false,
+      timeout: 60000
+    })
+    .done(function(datas){
+      console.log("done");
+      if ( $('#item_size_id').length == 0){
+        var formgroup =
+          `<div class="form-group form-group-size">
+            <label for="item_size_id">サイズ
+              <span class="form-require">必須</span>
+            </label>
+            <div class="select-wrap">
+              <select class="select-default" name="item[size_id]" id="item_size_id">
+                <option value="">---</option>
+              </select>
+              <i class="fa fa-angle-down"></i>
+            </div>
+            <ul class="has-error-text">
+            </ul>
+          </div>`
+        options = datas.size.map(function(data){
+          option = $('<option>',{value: data.id, text: data.name});
+          return option;
+        });
+        $('.form-group-category').after(formgroup);
+        $('#item_size_id').append(options);
 
+        if (datas.brand.flag){
+          var formgroup =
+            `<div class="form-group form-group-brand">
+              <label for="item_brand_id">ブランド
+                <span class="form-arbitrary">任意</span>
+              </label>
+              <div class="select-wrap">
+                <input class="input-default" id="item_brand" value="" placeholder="例）シャネル">
+              </div>
+            </div>`
+          $('.form-group-item_condition').before(formgroup);
+        }
+      }else{
+
+      }
+    })
+    .fail(function(){
+      console.log("fail");
+    })
+  });
 
     // 配送料の負担選択で配送方法
   $(document).on("change", "#item_postage_select_id", function (e) {
@@ -131,7 +192,7 @@ $(document).on('turbolinks:load', function() {
     .done(function(datas){
       if ( $('#item_shipping_id').length == 0){
         var formgroup =
-          `<div class="form-group">
+          `<div class="form-group form-group-shipping">
             <label for="item_shipping_id">配送方法
               <span class="form-require">必須</span>
             </label>
@@ -165,5 +226,18 @@ $(document).on('turbolinks:load', function() {
     .fail(function(){
       console.log("fail");
     })
+  });
+  $(document).on("keyup", "#item_price", function (e) {
+    var price = Number($(this).val());
+    if ((price >= 300) && (price < 10000000)) {
+      var fee =0.1; //手数料 10％
+      var commission = Math.floor(price * fee);
+      var profit = (price - commission);
+      $('.sell-commission-value').text("¥"+commission.toLocaleString());
+      $('.sell-profit-value').text("¥"+profit.toLocaleString());
+    } else {
+      $('.sell-commission-value').text("-");
+      $('.sell-profit-value').text("-");
+    }
   });
 });
