09a57395ac0198d275e923b8488b3941ab522573
kykt35 <kykt35@gmail.com>
2018-12-22 08:42:19 +0900

    Add #size_brand for ajax
    カテゴリ選択時にサイズとブランドを表示するためのレスポンス

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,4 +7,14 @@ class CategoriesController < ApplicationController
       }
     end
   end
+  def size_brand
+    respond_to do |format|
+      format.html
+      format.json{
+        m_category = Category.find(params[:id]).parent
+        @sizes = m_category.sizes
+        @has_brand = m_category.has_brand
+      }
+    end
+  end
 end
