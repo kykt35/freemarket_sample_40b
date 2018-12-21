a760fe998217ad854c815159cb8732a09a5ddc59
kykt35 <kykt35@gmail.com>
2018-12-21 12:54:24 +0900

    Edit category select action
    カテゴリを選択した時にサブカテゴリが表示される機能追加

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -14,4 +14,98 @@ $(document).on('turbolinks:load', function() {
     var i = $(this).index();
     $('.item-photo').slick('slickGoTo', i, false);}
   });
+
+  // カテゴリセレクトでサブカテゴリ追加
+  $('#item_l_category_id').on('change',function(e){
+    e.preventDefault();
+    var category_id = $('#item_l_category_id option:selected').val()
+    if (category_id == ""){
+      $('#item_m_category_id').parent().remove();
+      $('#item_category_id').parent().remove();
+      return;
+    }
+    $.ajax({
+      type: "GET",
+      url: '/categories',
+      data: {id: category_id},
+      dataType: 'json',
+      processData: true,
+      contentType: false,
+      timeout: 60000
+    })
+    .done(function(categories){
+      if ($('#item_category_id').length !=0) {
+        $('#item_category_id').parent().remove();
+      }
+      if ($('#item_m_category_id').length == 0){
+        var selectWrap =$('<div>',{class: 'select-wrap'});
+        var select = $('<select>',{class: "select-default", name: "item[m_category_id]", id:  "item_m_category_id"});
+        select.append(`<option value >---</option>`);
+        options = categories.map(function(category){
+          option = $('<option>',{value: category.id, text: category.name});
+          return option;
+        });
+        selectWrap.append(select.append(options));
+        selectWrap.append($('<i>',{class: 'fa fa-angle-down'}));
+        $('#item_l_category_id').parent().parent().append(selectWrap);
+      } else {
+        select = $('#item_m_category_id');
+        select.children().remove()
+        select.append(`<option value >---</option>`);
+        options = categories.map(function(category){
+          option = $('<option>',{value: category.id, text: category.name});
+          return option;
+        });
+        select.append(options);
+      }
+    })
+    .fail(function(){
+      console.log("fail");
+    })
+  });
+
+  // カテゴリセレクトでサブサブカテゴリ追加
+  $(document).on("change", "#item_m_category_id", function (e) {
+    e.preventDefault();
+    var category_id = $('#item_m_category_id option:selected').val()
+    if (category_id == ""){
+      $('#item_category_id').parent().remove();
+      return;
+    }
+    $.ajax({
+      type: "GET",
+      url: '/categories',
+      data: {id: category_id},
+      dataType: 'json',
+      processData: true,
+      contentType: false,
+      timeout: 60000
+    })
+    .done(function(categories){
+      if ( $('#item_category_id').length == 0){
+        var selectWrap =$('<div>',{class: 'select-wrap'});
+        var select = $('<select>',{class: "select-default", name: "item[category_id]", id:  "item_category_id"});
+        select.append(`<option value >---</option>`);
+        options = categories.map(function(category){
+          option = $('<option>',{value: category.id, text: category.name});
+          return option;
+        });
+        selectWrap.append(select.append(options));
+        selectWrap.append($('<i>',{class: 'fa fa-angle-down'}));
+        $('#item_m_category_id').parent().parent().append(selectWrap);
+      } else {
+        select = $('#item_category_id');
+        select.children().remove()
+        select.append(`<option value >---</option>`);
+        options = categories.map(function(category){
+          option = $('<option>',{value: category.id, text: category.name});
+          return option;
+        });
+        select.append(options);
+      }
+    })
+    .fail(function(){
+      console.log("fail");
+    })
+  });
 });
