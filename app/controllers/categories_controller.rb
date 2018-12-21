a86bc63e3f1ecfd9946de3e67bba8e3718b9a585
kykt35 <kykt35@gmail.com>
2018-12-21 12:52:39 +0900

    Edit categories controller
    json のレスポンスに移動

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,10 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.children_of(Category.find(params[:id]))
-
+    respond_to do |format|
+      format.html
+      format.json{
+         @categories = Category.children_of(Category.find(params[:id]))
+      }
+    end
   end
 end
