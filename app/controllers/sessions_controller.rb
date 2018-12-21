a51b184884602721852a04fcfad8bd66c540bf39
kukbkub <kukbkub.create.program@gmail.com>
2018-12-21 20:25:50 +0900

    Delete unnecessary method, and call User-class-method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,8 @@ class SessionsController  < Devise::OmniauthCallbacksController
   def create
     auth = request.env["omniauth.auth"]
     if auth.present?
-      unless @auth = SnsCredential.find_with_omniauth(auth)
-        @auth = SnsCredential.create_with_omniauth(auth)
-      end
-      @user = @auth.user
+      @credential = User.create_from_auth!(auth)
+      @user = @credential.user
       sign_in_and_redirect @user, event: :authentication
     else
       redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
