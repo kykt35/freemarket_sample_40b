a9f8c4f4aad28423737b7014f98c801c7d900226
kykt35 <kykt35@gmail.com>
2018-12-21 14:30:19 +0900

    Add association to postage_select

--- app/models/postage_select.rb
+++ app/models/postage_select.rb
@@ -1,3 +1,5 @@
 class PostageSelect < ApplicationRecord
   has_many :items
+  has_many :postage_selects_shippings, dependent :destroy
+  has_many :shippings, through: :postage_selects_shippings
 end
