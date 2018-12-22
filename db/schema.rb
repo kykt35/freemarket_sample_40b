fc20ae9da52039d13047b443b5a9df1a455a89b1
kykt35 <kykt35@gmail.com>
2018-12-22 09:30:10 +0900

    Add hasBrand column to categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_22_001748) do
+ActiveRecord::Schema.define(version: 2018_12_22_002620) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2018_12_22_001748) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ancestry"
+    t.boolean "hasBrand", default: false
     t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
