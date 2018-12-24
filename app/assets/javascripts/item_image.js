92ac642d9b4658ce5cd09a13f5b99fde5f679723
kykt35 <kykt35@gmail.com>
2018-12-25 00:06:34 +0900

    Add image upload function
    画像を登録する毎にサーバーにアップロードする。
    サーバーのキーをフォームに隠しキーを元に商品に紐つける。

---
+++ app/assets/javascripts/item_image.js
@@ -0,0 +1,48 @@
+$(document).on('turbolinks:load', function() {
+
+  function buildItemHTML(imageFile,key){
+    var reader = new FileReader();
+    var upload_items = $('.sell-upload-items ul li')
+    var item_image =
+      `<li class="sell-upload-item upload-item-${upload_items.length + 1}">
+        <input name="item[uploaded_images][]" value=${key} type="hidden" id="item_uploaded_image">
+        <figure class="sell-upload-figure">
+          <img src="" alt="" class="">
+        </figure>
+        <div class="sell-upload-button">
+          <a href="" class="sell-upload-edit">編集</a>
+          <a href="" class="sell-upload-delete">削除</a>
+        </div>
+      </li>`;
+
+    reader.onload = (function(imageFile){
+      return function(e){
+        $('.sell-upload-dropbox').removeClass().addClass("sell-upload-dropbox clearfix");
+        $('.sell-upload-dropbox').addClass("have-item-"+(upload_items.length+1));
+        $('.sell-upload-items ul').append(item_image);
+        $('.upload-item-'+ (upload_items.length + 1) + " img").attr("src", reader.result);
+        l = $('.upload-item-'+ (upload_items.length + 1 + " img"))
+      };
+    })(imageFile);
+    reader.readAsDataURL(imageFile);
+  }
+
+  function uploadImage(imageFile){
+    var formData = new FormData(this);
+    formData.append('image', imageFile);
+    $.ajax({
+      type: "POST",
+      url: '/items/upload_image',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      buildItemHTML(imageFile,data.image.key)
+    })
+    .fail(function(){
+      alert('サーバーとの通信に失敗しました');
+    })
+  }
+});
