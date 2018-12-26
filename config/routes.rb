6f1e36225003ecaf0d5cf2eae40e8b542d0858b9
kanchimatsumoto <kanchi0707@outlook.jp>
2018-12-26 16:12:40 +0900

    edit_routing

--- config/routes.rb
+++ config/routes.rb
@@ -6,16 +6,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
   get 'mypage' => 'users#mypage'
-  get 'mypage/card' => 'users#card'
-  get 'mypage/card/add' => 'users#add'
+   post 'mypage/card' => 'credits#create'
+  get 'mypage/card' => 'credits#index'
+  get 'mypage/card/new' => 'credits#new'
   get 'mypage/profile' => 'users#profile'
   get 'mypage/identification', to: 'users#identification'
   get 'users/signup', to: 'users#signup'
   get 'users/logout', to: 'users#logout'
   get 'users/registration_card' => 'users#registration_card'
-  resources :items, only: [:new, :show, :create, :destroy, :edit, :update]
-  resources :transaction, only: [:new]
-  get 'mypage/identification', to: 'users#identification'
+  resources :items, only: [:new, :show, :create, :destroy, :edit, :update] do
+    resources :transaction, only: [:new, :create] do
+    end
+  end
   resources :categories, only: [:index]
   resources :postage_selects, only: [:index]
   get 'categories/size_brand', to: 'categories#size_brand'
