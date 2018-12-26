da1f195c9e861c58ef5d02cbb240cc0d86f5daef
Ttoshinari <freeworker1105@gmail.com>
2018-12-26 14:34:05 +0900

    コメント削除　controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,12 @@ class CommentsController < ApplicationController
     redirect_to @comment.item
   end
 
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy if comment.user_id == current_user.id
+    redirect_to item_path(comment.item)
+  end
+
   private
   def comment_params
     params.permit(:text,:item_id).merge(user_id: current_user.id)
