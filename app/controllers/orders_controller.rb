62f8126663556ac0f93915b89dbf9d2f68bd7b14
kanchimatsumoto <kanchi0707@outlook.jp>
2018-12-26 20:03:43 +0900

    create_order_controller_and_model

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,26 @@
+class OrdersController < ApplicationController
+    before_action :set_item, only: [:new, :pay]
+
+  def new
+  end
+
+  def create
+    @item = Item.find(params[:id])
+    price = @item.price
+    if current_user && current_user.credits.present
+       charge(price)
+    end
+    @order = Order.new(order_params)
+    @sales = Sales.new(price: price * 0.9, limit_time: Date.today() + "180", user_id: @item.user_id)
+    redirect_to root_path, notice: 'ありがとうございました。'
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  private
+  def order_params
+    params.permit(:order).permit(:item_id, :customer_id)
+  end
+end
---
+++ app/models/order.rb
@@ -0,0 +1,3 @@
+class Order < ApplicationRecord
+  belongs: items
+end
