480bdd99eaa73a8e28ddfcdba1df184d4936d9a1
kanchimatsumoto <kanchi0707@outlook.jp>
2018-12-27 17:33:56 +0900

    Enum型にした

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,8 @@ class Item < ApplicationRecord
   belongs_to :postage_select
   belongs_to :leadtime
   belongs_to :prefecture
-  has_many :transactions
+  has_many :item_transactions
+  enum status:[:active, :sold, :finished, :freezed]
   validates :name, :description, :category_id, :item_condition_id, :postage_select_id, :shipping_id, :prefecture_id, :leadtime_id, :price, :seller_id, presence: true
   validates :price, numericality: { only_integer: true }
   validates :name, presence: true, length: { maximum: 40 }
