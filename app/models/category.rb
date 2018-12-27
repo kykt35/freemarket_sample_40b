4b0d942c41b4395e0ad7a4720f79962eb00d5e9d
kykt35 <kykt35@gmail.com>
2018-12-27 18:07:57 +0900

    Add all_item method to Category
    子要素のitem全てを取得するメソッドを追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,18 @@ class Category < ApplicationRecord
   has_many :items
   has_many :categories_sizes, dependent: :destroy
   has_many :sizes, through: :categories_sizes
+
+  def all_items
+    items_array = []
+    if has_children?
+      children.each do |child|
+        items_array << child.items unless child.items.empty?
+        items_array << child.all_items
+      end
+    else
+      items_array << items
+    end
+    return items_array.flatten
+  end
+
 end
