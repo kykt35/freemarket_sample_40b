636ef27871d3d43059c559f2e9fa5b4ac6b34035
kykt35 <kykt35@gmail.com>
2018-12-28 22:38:27 +0900

    Add likes#create, destroy action

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,13 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :destroy]
+
   def create
+    @item = Item.find(params[:item_id])
+    @item.like(current_user)
   end
 
   def destroy
+    @item =Item.find(params[:item_id])
+    @item.unlike(current_user);
   end
 end
