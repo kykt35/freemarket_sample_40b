9f3433ca825d54062bf93b97a71ec3bdbdd4ccc6
kykt35 <kykt35@gmail.com>
2018-12-30 05:02:18 +0900

    Edit favorite model test

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -2,33 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
   describe '#create' do
+
     context 'can save' do
       it 'is valid with a user, item' do
-        favorite = build(:favorite)
+        user = create(:user)
+        seller = create(:user)
+        item = create(:item, :image, seller: seller)
+        favorite = Favorite.new(user: user, item: item)
         expect(favorite).to be_valid
-      end
 
-      it 'is invalid without user' do
-        favorite = build(:favorite, user: nil)
-        favorite.valid?
-        expect(favorite.errors[:user]).to include("must exist")
-      end
-      it 'is invalid without item' do
-        favorite = build(:favorite, item: nil)
-        favorite.valid?
-        expect(favorite.errors[:item]).to include("must exist")
-      end
-      it 'is invalid without user and item' do
-        favorite = build(:favorite, user: nil, item: nil)
-        favorite.valid?
-        expect(favorite.errors[:user_id]).to include("must exist")
-        expect(favorite.errors[:item_id]).to include("must exist")
-      end
-      it 'is invalid ' do
-        favorite1 = create(:favorite)
-        favorite2 = build(:favorite)
-        favorite2.valid?
-        expect(favorite2.errors[:user]).to include("must exist")
       end
     end
   end
