73c068c19cd2a17de91e7f2e38b79d2231faa9fc
kykt35 <kykt35@gmail.com>
2018-12-29 22:37:53 +0900

    Create likes_spec

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    context 'can save' do
+      it 'is valid with a user, item' do
+        like = build(:like)
+        expect(like).to be_valid
+      end
+      it 'is invalid without user and item' do
+        like = build(:like, user: nil, item: nil)
+        like.valid?
+        expect(like.errors[:user]).to include("must exist")
+        expect(like.errors[:item]).to include("must exist")
+      end
+      it 'is invalid without user' do
+        binding.pry
+        like = build(:like, user: nil)
+        like.valid?
+        expect(like.errors[:user]).to include("must exist")
+      end
+      it 'is invalid without item' do
+        like = build(:like, item: nil)
+        like.valid?
+        expect(like.errors[:item]).to include("must exist")
+      end
+      it 'is invalid ' do
+        like1 = create(:like)
+        like2 = build(:like)
+        like2.valid?
+        expect(like2.errors[:user]).to include("must exist")
+      end
+    end
+  end
 end
