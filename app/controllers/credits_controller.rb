663501d01d5a91d9b17880134af6a330e97d2618
kanchimatsumoto <kanchi0707@outlook.jp>
2019-01-07 12:50:08 +0900

    Flash

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -12,17 +12,19 @@ class CreditsController < ApplicationController
   def create
     @credit = current_user.credits.new(customer_id: create_customer) #token.idのcustomerのidを取得し、Creditモデルのカラムのcustomer_idと定義する
     if @credit.save
+     flash[:notice] = "カードを追加しました。"
      redirect_to credits_path
     else
       render 'new' # カード未登録で保存に失敗
     end
   end
 
-    def destroy
-      credit = current_user.credits.first
-      credit.destroy if user_signed_in? && current_user.credits.present?
-      redirect_to credits_path
-    end
+  def destroy
+    credit = current_user.credits.first
+    credit.destroy if user_signed_in? && current_user.credits.present?
+    flash[:notice] = "カードを追加しました。"
+    redirect_to credits_path
+  end
 
 end
 
