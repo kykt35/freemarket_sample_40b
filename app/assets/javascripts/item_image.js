bbbd0203f96ded453ae1dd540d077c59cd6da9a1
kykt35 <kykt35@gmail.com>
2019-01-08 15:31:37 +0900

    Edit item_image.js to add resize image
    ブラウザで画像リサイズする処理を追加
    canvasに一度描写し、canvasから画像を作成することでリサイズし容量を低減させる。

--- app/assets/javascripts/item_image.js
+++ app/assets/javascripts/item_image.js
@@ -1,4 +1,5 @@
 $(document).on('turbolinks:load', function() {
+  var blob = null;
 
   function buildItemHTML(imageFile,key){
     var reader = new FileReader();
@@ -45,11 +46,60 @@ $(document).on('turbolinks:load', function() {
     })
   }
 
+  function resizeImage(file) {
+  const WIDTH = 360;
+  const HEIGHT = 360;
+  var img = new Image();
+  var def =$.Deferred();
+  var canvas = document.createElement('canvas');
+  var reader = new FileReader();
+  reader.onload = function(e){
+    img.onload = function(){
+      if(img.width > img.height){
+        var ratio = img.height/img.width;
+        width = WIDTH;
+        height = HEIGHT * ratio;
+      } else {
+        var ratio = img.width/img.height;
+        width = HEIGHT * ratio;
+        height = HEIGHT;
+      }
+
+      // canvasに描写
+      var ctx = canvas.getContext('2d');
+      canvas.width = width;
+      canvas.height = height;
+      ctx.drawImage(img,0,0,width,height);
+
+      // base64からBlobデータを作成
+      var base64 = canvas.toDataURL('image/jpeg');
+      var barr, bin, i, len;
+      bin = atob(base64.split('base64,')[1]);
+      len = bin.length;
+      barr = new Uint8Array(len);
+      i = 0;
+      while (i < len) {
+        barr[i] = bin.charCodeAt(i);
+        i++;
+      }
+      blob = new Blob([barr], {type: 'image/jpeg'});
+      def.resolve(canvas);
+    };
+    img.src = e.target.result;
+  };
+  reader.readAsDataURL(file);
+  return def.promise();
+  }
+
   $('.sell-upload-drop-file').on('change',function(e){
     var files = e.target.files;
+
     for (var i = 0; i < files.length; i++) {
       if ($('.sell-upload-item').length <= 10){
-        uploadImage(files[i]);
+        file = files[i];
+        resizeImage(file).then(function(){
+          uploadImage(blob);
+        });
       }
     }
     $('.sell-upload-drop-file').val('');
